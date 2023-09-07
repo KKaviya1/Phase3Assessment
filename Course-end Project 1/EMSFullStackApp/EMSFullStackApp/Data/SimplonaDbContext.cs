ea896ec696e162de4d3ab357d8cd7a7d1221e5da
KKaviya1 <Kaviyagandhi136@gmail.com>
2023-09-07 12:19:36 +0530

    my commit

---
+++ Course-end Project 1/EMSFullStackApp/EMSFullStackApp/Data/SimplonaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EMSFullStackApp.Models;
+
+namespace EMSFullStackApp.Data
+{
+    public class SimplonaDbContext : DbContext
+    {
+        public SimplonaDbContext (DbContextOptions<SimplonaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EMSFullStackApp.Models.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<EMSFullStackApp.Models.EmpProfile>? EmpProfile { get; set; }
+    }
+}
